b7cfd23be543e2a50aba6ff35252b15f235c44ee
kalc1 <kalcocer1@gmail.com>
2023-09-20 20:56:49 -0700

    add new .py file

---
+++ Code Challenge File IO.py
@@ -0,0 +1,9 @@
+# Code Challenge File IO
+#
+# Python file name: __Code Challenge File IO.py____
+#
+# Date: ___09/20/23______
+#
+# Programmer's name: __Kevin_Alcocer___
+#
+# ********************************************************************************************
