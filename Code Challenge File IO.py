f06108c6ea249dca4d95e2beaf364d26bdb04fd8
kalc1 <kalcocer1@gmail.com>
2023-09-23 14:39:15 -0700

    revert back to using .strip() method to append each animal to list

--- Code Challenge File IO.py
+++ Code Challenge File IO.py
@@ -15,14 +15,17 @@ read_Animals = arrivingAnimals.readlines()
 
 # This code will take the above and use .strip() to remove the escape characters "\n" while adding it to a list. Each line in the .txt file is now a list item. 
 # Adding a .split("'") function turns the Animals_list into a list of lists. This should make it easier to work with and split into 
-# Animals_list = [] <- I originally started by building a list and adding each element from read_Animals to this
+Animals_list = []               # <- I originally started by building a list and adding each element from read_Animals to this
 for animal in read_Animals: 
-    animal_split = animal("'")
-    print(animal_split)
-
+    Animals_list.append(animal.strip())
+for animal in Animals_list:
+    print(animal)
+    
 # Each line should contain 6 substrings: 1.Animal 2.Season Born 3.Color 4.Weight 5.From 6. Country
 # Will need to use split(',') at some point in order to make the above happen. <- Makes a list out of a string 
 
 # Using this I can now see each line from the text file printed out on seperate lines, with each line being a list I can manipulate.  
 
-    
+#outputFile = open('outputFile.txt', 'w')    
+#outputFile.write(animal_split)
+#outputFile.close()
