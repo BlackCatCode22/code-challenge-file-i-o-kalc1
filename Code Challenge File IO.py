f98099743bfe645353edf0b1d06c9c9502b7a9e8
kalc1 <kalcocer1@gmail.com>
2023-09-22 21:31:19 -0700

    use .split("'") to seperate each line in the text file into a seperate list

--- Code Challenge File IO.py
+++ Code Challenge File IO.py
@@ -14,11 +14,12 @@ read_Animals = arrivingAnimals.readlines()
 #print(read_Animals)
 
 # This code will take the above and use .strip() to remove the escape characters "\n" while adding it to a list. Each line in the .txt file is now a list item. 
+# Adding a .split("'") function turns the Animals_list into a list of lists. This should make it easier to work with and split into 
 Animals_list = []
-for animal in read_Animals:
-    Animals_list.append(animal.strip())
-print (Animals_list)
-
+for animal in read_Animals: 
+    animal_strip = animal.strip()
+    Animals_list.append(animal_strip.split("'"))
 
 # Each line should contain 6 substrings: 1.Animal 2.Season Born 3.Color 4.Weight 5.From 6. Country
 # Will need to use split(',') at some point in order to make the above happen. <- Makes a list out of a string 
+
