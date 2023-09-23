7ad559ac959128dbb68ca77b4a95f422d09068a9
kalc1 <kalcocer1@gmail.com>
2023-09-23 14:17:20 -0700

    create next .txt file for outputs named outputFile.txt

--- Code Challenge File IO.py
+++ Code Challenge File IO.py
@@ -17,7 +17,8 @@ read_Animals = arrivingAnimals.readlines()
 # Adding a .split("'") function turns the Animals_list into a list of lists. This should make it easier to work with and split into 
 # Animals_list = [] <- I originally started by building a list and adding each element from read_Animals to this
 for animal in read_Animals: 
-    print(animal)
+    animal_split = animal("'")
+    print(animal_split)
 
 # Each line should contain 6 substrings: 1.Animal 2.Season Born 3.Color 4.Weight 5.From 6. Country
 # Will need to use split(',') at some point in order to make the above happen. <- Makes a list out of a string 
